cbdc5eed80f38708a7efd863adb09e2a4afee2f5
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-07-29 09:38:54 +0900

    7,8 gpg key

--- scripts/04_base_install.sh
+++ scripts/04_base_install.sh
@@ -4,6 +4,8 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 source "$SCRIPT_DIR/00_env.sh"
 
 echo "[+] Setting up musl repository config..."
+mkdir -p /mnt/var/db/xbps/keys
+cp -a /var/db/xbps/keys/* /mnt/var/db/xbps/keys/
 cp ./assets/xbps_arch.sh /etc/profile.d
 source /etc/profile.d/xbps_arch.sh
 mkdir -p /mnt/etc/xbps.d
